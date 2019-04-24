adf175689ad3c8c9e5705188936c0cec0221e07f
GuiMac <guibasconti@gmail.com>
2019-04-24 17:15:00 -0300

    Create card components

---
+++ src/components/card/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <img :src="image">
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  components: {},
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    action: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    image: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    description: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    value: {
+      type: Number,
+      required: false,
+      default: 0
+    }
+  }
+};
+</script>
