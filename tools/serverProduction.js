433c0a4ec140749cf95d61aed9cd1418cde6278f
Guilherme Conti <guibasconti@gmail.com>
2019-06-23 19:57:18 -0300

    Express listening to other routes

--- tools/serverProduction.js
+++ tools/serverProduction.js
@@ -7,5 +7,6 @@ const express = require('express');
 const app = express();
 
 app.use('/', express.static('dist'));
+app.use(['/rooms/*', '/games/*'], express.static('dist/index.html'));
 
 module.exports = app;
