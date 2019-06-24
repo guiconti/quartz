83cb2fdc3dc7961c25b36a4b275bf7b677c86cab
Guilherme Conti <guibasconti@gmail.com>
2019-06-24 00:29:21 -0300

    Call gateway for APIs

--- src/utils/API.js
+++ src/utils/API.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'mock') {
 }
 
 export default axios.create({
-  baseURL: process.env.BACKEND_HOST,
+  baseURL: process.env.GATEWAY_HOST,
   withCredentials: true,
   timeout: 100000,
   headers: {
